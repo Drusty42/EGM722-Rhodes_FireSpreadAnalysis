be4641bae05ab37340036ddfc1ce5af3e2e20c64
Drusty42 <129114690+Drusty42@users.noreply.github.com>
2023-08-06 17:50:18 +0100

    Today i have finalised the map product with feature symbology, legend, title and output to a png graphic. I have had great difficulty and no success with adding a scale bar and grid. I suspect it is an coordinate system issue, even though UTM zone 35 is defined. I have removed attempts from my script. The final thing i would like to achieve is a buffer applied to the fire dataset to highlight a danger area, possibly used to visualise BUA at risk or needs to be evacuated, and the surrounding routes and water sources. Finally i will add the docstings to explain my script in more detail.

--- Python Code.py
+++ Python Code.py
@@ -6,7 +6,7 @@ import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
-# Create a dictionary to assign fire dates to colors and sizes
+# creates a dictionary to assign fire dates to colors and sizes
 fire_styles = {
     '24/07/2023': {'colour': 'yellow', 'size': 6},
     '25/07/2023': {'colour': 'darkorange', 'size': 6},
@@ -16,56 +16,34 @@ fire_styles = {
 
 }
 
-# Create a dictionary to assign BUA classes to colors and symbols
+# creates a dictionary to assign BUA classes to colors, symbols and sizes
 bua_styles = {
-    'city': {'colour': 'dimgrey', 'marker': '*', 'size': 17},
-    'town': {'colour': 'dimgrey', 'marker': 's', 'size': 7},
-    'suburb': {'colour': 'dimgrey', 'marker': 's', 'size': 5},
+    'city': {'colour': 'purple', 'marker': '*', 'size': 17},
+    'town': {'colour': 'purple', 'marker': 's', 'size': 7},
+    'suburb': {'colour': 'purple', 'marker': 's', 'size': 5},
     'village': {'colour': 'k', 'marker': 's', 'size': 4},
     'hamlet': {'colour': 'k', 'marker': 's', 'size': 2.5},
     'locality': {'colour': 'k', 'marker': 's', 'size': 1},
 
 }
 
-# Create a dictionary to assign road types to colors and widths
+# creates a dictionary to assign road types to colors and widths
 road_type_styles = {
     'primary': {'colour': 'red', 'width': 3.0},
-    'secondary': {'colour': 'orange', 'width': 1.5},
-    'tertiary': {'colour': 'brown', 'width': 1.0},
+    'secondary': {'colour': 'green', 'width': 1.5},
+    'tertiary': {'colour': 'lime', 'width': 1.0},
 
     }
 
 # generates matplotlib handles to create a legend of features to be used within the map
-def generate_handles(labels, colors, edge='k', alpha=1):
+def generate_handles(labels, colors, halo_color='white', edge='k', alpha=1):
     lc = len(colors)  # get the length of the color list
     handles = []
     for i in range(len(labels)):
-        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
+        # Create a rectangle with white halo and specified face color
+        rect = mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha, linewidth=0.5, linestyle='dashed', hatch='////', joinstyle='round')
+        handles.append(rect)
     return handles
-
-# Create a scale bar of 25Km length with 5Km increments located bottom right of the map
-def scale_bar(ax, location=(0.92, 0.05)):
-    x0, x1, y0, y1 = ax.get_extent()
-    sbx = x0 + (x1 - x0) * location[0]
-    sby = y0 + (y1 - y0) * location[1]
-
-    ax.plot([sbx, sbx - 25000], [sby, sby], color='k', linewidth=9, transform=ax.transData)
-    ax.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=ax.transData)
-    ax.plot([sbx, sbx - 15000], [sby, sby], color='k', linewidth=9, transform=ax.transData)
-    ax.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=9, transform=ax.transData)
-    ax.plot([sbx, sbx - 5000], [sby, sby], color='k', linewidth=6, transform=ax.transData)
-    ax.plot([sbx - 5000, sbx - 10000, sbx - 15000, sbx - 20000, sbx - 25000], [sby, sby], color='w', linewidth=6,
-            transform=ax.transData)
-
-    ax.text(sbx - 25000, sby - 4500, '25 km', transform=ax.transData, fontsize=8, ha='left')
-    ax.text(sbx - 20000, sby - 4500, '20 km', transform=ax.transData, fontsize=8, ha='left')
-    ax.text(sbx - 15000, sby - 4500, '15 km', transform=ax.transData, fontsize=8, ha='left')
-    ax.text(sbx - 10000, sby - 4500, '10 km', transform=ax.transData, fontsize=8, ha='left')
-    ax.text(sbx - 5000, sby - 4500, '5 km', transform=ax.transData, fontsize=8, ha='left')
-    ax.text(sbx, sby - 4500, '0 km', transform=ax.transData, fontsize=8, ha='left')
-
-# loads the boundary of Rhodes island as the land extent and analysis area
-outline = gpd.read_file(os.path.abspath('AssessmentData/RhodesBnd.shp'))
 
 # loads the datasets to be displayed on the map
 BUA = gpd.read_file(os.path.abspath('AssessmentData/BUA.shp'))
@@ -80,19 +58,31 @@ myFig = plt.figure(figsize=(10, 10))
 # creates a Universal Transverse Mercator (UTM) coordinate system to transform data
 myCRS = ccrs.UTM(35)
 
+# loads the boundary of Rhodes island as the land extent and analysis area
+outline = gpd.read_file(os.path.abspath('AssessmentData/RhodesBnd.shp'))
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='sienna', linewidth=1)
+
 # create axis object in the figure using the UTM projection to plot data.
 ax = plt.axes(projection=myCRS)
 
-# Draw BUA features with hierarchical symbology
+
+# draws BUA features with hierarchical symbology
 bua_legend_handles = []
+
 for bua_class in bua_styles.keys():
     style = bua_styles[bua_class]
     bua_class_features = BUA[BUA['fclass'] == bua_class]
-    ax.plot(bua_class_features['geometry'].x, bua_class_features['geometry'].y, marker=style['marker'], color=style['colour'], linestyle='None', markersize=style['size'], label=bua_class, zorder=5)
+
+    # Plot the white-filled BUA features for the halo effect
+    ax.plot(bua_class_features['geometry'].x, bua_class_features['geometry'].y, marker=style['marker'], color='white', linestyle='None', markersize=style['size'] + 1, zorder=5)
+
+    # Plot the actual BUA features
+    ax.plot(bua_class_features['geometry'].x, bua_class_features['geometry'].y, marker=style['marker'], color=style['colour'], linestyle='None', markersize=style['size'], label=bua_class, zorder=6)
+
+    # Add legend handles
     bua_legend_handles.append(mlines.Line2D([], [], color=style['colour'], marker=style['marker'], markersize=style['size'], linestyle='None', label=bua_class))
 
-
-# Sort road features into a hierarchy and draw in that order
+# Draws road features with a set hierarchy to draw in that order
 road_legend_handles = []
 for road_type_name in ['primary', 'primary_link', 'secondary', 'secondary_link', 'tertiary', 'track']:
     if road_type_name in road_type_styles:
@@ -102,29 +92,28 @@ for road_type_name in ['primary', 'primary_link', 'secondary', 'secondary_link',
             ax.add_line(mlines.Line2D(*geom.xy, color=style['colour'], linewidth=style['width'], zorder=4))
         road_legend_handles.append(mlines.Line2D([], [], color=style['colour'], linewidth=style['width'], label=road_type_name))
 
-# Draw fire points with symbology defined by acquisition date and apply transparency
+# Draws fire points with symbology defined by acquisition date and apply transparency
 fire_legend_handles = []
 for acq_date in fire_styles.keys():
     style = fire_styles[acq_date]
     fire_date_features = Fire[Fire['ACQDATENEW'] == acq_date]
     if not fire_date_features.empty:
         for geom in fire_date_features['geometry']:
-            ax.plot(geom.x, geom.y, marker='o', color=style['colour'], markersize=style['size'], markeredgecolor=style['colour'], linestyle='None', label=acq_date, zorder=6, alpha=0.4)
-        fire_legend_handles.append(mlines.Line2D([], [], color=style['colour'], marker='o', markersize=style['size'], linestyle='None', label=acq_date, alpha=0.4))
+            ax.plot(geom.x, geom.y, marker='o', color=style['colour'], markersize=style['size'],
+                    markeredgecolor=style['colour'], linestyle='None', label=acq_date, zorder=7, alpha=0.25)
+        fire_legend_handles.append(mlines.Line2D([], [], color=style['colour'], marker='o', markersize=style['size'],
+                                                 linestyle='None', label=acq_date, alpha=0.5))
 
-# add the Rhodes boundary
-outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='khaki')
-
-# Draw river lines
-river_color = 'blue'
-river_size = 1.5
+# Draws river lines
+river_color = 'deepskyblue'
+river_size = 0.5
 river_legend_handles = [mlines.Line2D([], [], color=river_color, linewidth=river_size, label='River')]
 
 for geom in River['geometry']:
     x, y = geom.xy
     ax.plot(x, y, color=river_color, linewidth=river_size, zorder=2)
 
-# Draw lake polygons
+# Draws lake polygons
 lake_color = 'darkblue'
 lake_features = Lake
 lake_shapes = [geom for geom in lake_features['geometry']]
@@ -132,12 +121,12 @@ lake_feature = ShapelyFeature(lake_shapes, myCRS, facecolor=lake_color, label='L
 ax.add_feature(lake_feature)
 lake_legend_handles = [mpatches.Patch(facecolor=lake_color, label='Lake')]
 
-# adds the created features to the map
+# adds the created features to the map.
 xmin, ymin, xmax, ymax = outline.total_bounds
 ax.add_feature(outline_feature)
 
 # using the boundary of the shapefile features, zoom the map to the area of interest
-ax.set_extent([xmin-0.1, xmax+0.1, ymin-0.1, ymax+0.1], crs=myCRS)
+ax.set_extent([xmin-0.15, xmax+0.01, ymin-0.01, ymax+0.01], crs=myCRS)
 
 # Add a colored background
 ax.add_patch(plt.Rectangle((xmin - 5000, ymin - 5000), xmax - xmin + 10000, ymax - ymin + 10000, facecolor='lightblue'))
@@ -168,4 +157,10 @@ legend_handles.extend(lake_legend_handles)
 # Create the legend
 plt.legend(handles=legend_handles, title='Legend', loc='upper left')
 
-plt.show()
+# Add a title text box to the top center of the map
+title_text = "Rhodes Wildfire Progression 24-27 Aug 2023"
+bbox_props = dict(boxstyle="square,pad=0.3", facecolor='white', edgecolor='black', linewidth=1)
+ax.text(0.5, 0.95, title_text, transform=ax.transAxes, fontsize=12, ha='center', va='center', bbox=bbox_props)
+
+# save the figure as map.png, cropped to the axis (bbox_inches='tight'), and a dpi of 300
+myFig.savefig('RhodesFireMap.png', bbox_inches='tight', dpi=300)
